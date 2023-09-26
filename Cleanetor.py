dceaf2ee51474e72010796106dc15e8ee18dd540
Facundo Batista <facundo@taniquetil.com.ar>
2023-09-26 18:58:41 -0300

    Limpiezas varias.

--- Cleanetor.py
+++ Cleanetor.py
@@ -1,5 +1,3 @@
-import csv
-import pathlib
 import sys
 import textwrap
 
@@ -18,7 +16,7 @@ from PyQt6.QtWidgets import (
     QWidget,
     QVBoxLayout,
     QHBoxLayout,
-    QLineEdit, 
+    QLineEdit,
 
 )
 
@@ -30,44 +28,31 @@ class MainPanel(QWidget):
         super().__init__()
         self.main_window = main_window
 
-
         layoutV = QVBoxLayout()
         self.setLayout(layoutV)
 
-
         self.label = QLabel("Directorio:",)
-        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layoutV.addWidget(self.label)
-
 
         layoutH1 = QHBoxLayout()
         layoutV.addLayout(layoutH1)
 
         layoutH2 = QHBoxLayout()
-        layoutV.addLayout(layoutH2)
-
+        layoutV.addLayout(layoutH2, stretch=1)
 
         self.label2 = QLabel("Tamaño Limite:")
-        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layoutH1.addWidget(self.label2)
-        
+
         self.edit = QLineEdit(self)
-        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        layoutH1.addWidget(self.edit)
+        layoutH1.addWidget(self.edit, stretch=1)
 
         self.button1 = QPushButton("Elegir Directorio")
         self.button1.clicked.connect(self.main_window.on_open)
         layoutH2.addWidget(self.button1)
 
         self.button2 = QPushButton("Empezar")
-        self.button2.clicked.connect(self.main_window.Archivos)
+        self.button2.clicked.connect(self.main_window.busca_archivos)
         layoutH2.addWidget(self.button2)
-        
-
-        #button = QPushButton("Abrir Directorio")
-        #button.clicked.connect(self.main_window.on_open)
-        #layout.addWidget(button)
-
 
 
 class Window(QMainWindow):
@@ -75,10 +60,9 @@ class Window(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.resize(400, 300)
+        self.resize(400, 200)
         self.setWindowTitle("Aplicacion Para Mami")
         self.setWindowIcon(QIcon("icon.png"))
-  
 
         # menú y barra de herramientas
         toolbar = QToolBar("main-toolbar")
@@ -87,11 +71,9 @@ class Window(QMainWindow):
         self.setStatusBar(self._status_bar)
         menubar = self.menuBar()
 
-        
-
         # las diferentes acciones tanto para el menu como para la barra de herramientas
-        open_action = QAction(QIcon.fromTheme("document-open"), "Abrir archivo", self)
-        open_action.setToolTip("Abrir archivo de datos")
+        open_action = QAction(QIcon.fromTheme("folder-open"), "Abrir directorio", self)
+        open_action.setToolTip("Elegir directorio")
         open_action.triggered.connect(self.on_open)
         open_action.setShortcut('Ctrl+A')
         quit_action = QAction(QIcon(), "Salir", self)
@@ -100,7 +82,7 @@ class Window(QMainWindow):
         about_action.triggered.connect(self.on_about)
 
         # configuramos la barra de herramientas
-        #toolbar.addAction(open_action)
+        toolbar.addAction(open_action)
 
         # configuramos el menú
         menu = menubar.addMenu("&Archivo")
@@ -113,7 +95,6 @@ class Window(QMainWindow):
         self.setCentralWidget(self.main_panel)
 
         self.set_status("Comenzando")
-
 
     def set_status(self, message):
         """Muestra un mensaje efímero en la barra de estado."""
@@ -128,18 +109,14 @@ class Window(QMainWindow):
         """Abre un nuevo archivo de datos y refresca el widget principal."""
         self.set_status("Abriendo archivo con datos")
 
-
         # abrimos un diálogo para que se seleccione un archivo
-        self.dirpath = QFileDialog.getExistingDirectory(self, "Abrir archivo")
-        print("============dirpath",repr(self.dirpath))
+        self.dirpath = QFileDialog.getExistingDirectory(self, "Elija un directorio")
+        print("============dirpath", repr(self.dirpath))
         if not self.dirpath:
             self.set_status("Ningún archivo seleccionado")
             return
-        
-        self.main_panel.label.setText("Directorio:" + self.dirpath)
 
-        
-
+        self.main_panel.label.setText("Directorio: " + self.dirpath)
 
     def on_about(self):
         """Muestra el diálogo de Acerca de."""
@@ -150,28 +127,20 @@ class Window(QMainWindow):
         """)
         QMessageBox.about(self, title, text)
 
-    def Archivos(self):
-        
-        max_archivo=int(self.main_panel.edit.text())
-  
-        for path,directorios,archive in os.walk(self.dirpath):
+    def busca_archivos(self):
+
+        max_archivo = int(self.main_panel.edit.text())
+
+        for path, directorios, archive in os.walk(self.dirpath):
             for archivo in archive:
-                #try:               
-                print("===Path Entradas",self.main_panel.edit.text(), "===")
-                filepath = os.path.join(path, archivo) 
+                print("===Path Entradas", self.main_panel.edit.text(), "===")
+                filepath = os.path.join(path, archivo)
                 entradas_info = os.stat(filepath)
                 print(entradas_info)
-                if entradas_info.st_size > max_archivo:    
+                if entradas_info.st_size > max_archivo:
                     print("===Tamaño===", entradas_info.st_size, filepath)
                 else:
                     print("Todo en orden")
-
-                #except:
-                #    print("Hubo un problema")
-
-            
-
-        
 
 
 app = QApplication(sys.argv)
